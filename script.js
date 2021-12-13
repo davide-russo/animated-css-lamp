6c2f317160aae4951467835c853e00d774687b7f
davide-russo <davide.russo.genova@gmail.com>
2021-12-13 21:51:05 +0100

    fix: arrange codebase to work on Stackblitz.com

--- script.js
+++
@@ -1,5 +0,0 @@
-const root = document.querySelector(':root');
-const lightColorPicker = document.getElementById('lightColorPicker');
-const lightColorButton = document.getElementById('lightColorButton');
-const openColorPicker = () =>  lightColorPicker.click();
-const updateLightColor = () =>  root.style.setProperty('--light-color', lightColorPicker.value);
